9221867b0cb6dc2da90b782d23a9f5dd44c9b8fe
Giancarlo Rocha <giancarloiff@gmail.com>
2020-07-14 09:27:50 -0300

    prevent github from caching images

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import isfile, join, realpath, dirname
 from random import choice
 
+
 app = Flask(__name__)
 
 def get_gif():
@@ -12,6 +13,12 @@ def get_gif():
 @app.route('/gif')
 def gif():
     return send_file(get_gif(), mimetype='image/gif')
+
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 0
+    response.cache_control.no_cache = True
+    return response
 
 if __name__ == '__main__':
     app.run()
