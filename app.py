075a99778198ee2e996aac725477ccea2ff72b98
Giancarlo Rocha <giancarloiff@gmail.com>
2020-07-14 09:40:32 -0300

    add etag to response

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def add_header(response):
     response.cache_control.max_age = 0
     response.cache_control.no_cache = True
     response.cache_control.private = True
+    response.add_etag()
     return response
 
 if __name__ == '__main__':
